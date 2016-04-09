3c97d159b04bb87bf1e1ad4fc3ac20e685473ff4
Hinostroza <Hinostroza>
2016-04-09 00:31:50 -0300

    Rompiendo Main

--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String [] args){
-		int primero;
-		int segundo=0;
-		int resultado;
-		String operador=null;
-		char op;
-		Nodo comienzo=new Nodo(0,null);
-		Nodo actual=new Nodo(0,null);
-		Scanner entrada=new Scanner(System.in);
-		String operacion=entrada.nextLine();
 		
-		StringTokenizer token=new StringTokenizer(operacion," ");
-		
-		String p = token.nextToken();
-		
-		primero=Integer.parseInt(p);
-		resultado=primero;
 		
 		Boolean flag=false;
 		
